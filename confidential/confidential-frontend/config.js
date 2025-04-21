a713ce9501229db58be7b90310a2ed05232638f2
高天逸_技術整合組 <max.ti.kao@mail.mli.com.tw>
2025-04-21 17:16:06 +0800

    info: 改名DemoRealm

--- confidential/confidential-frontend/config.js
+++ confidential/confidential-frontend/config.js
@@ -1,6 +1,6 @@
 window.CONFIG = {
     CLIENTID: "test",
-    REALM: "MLIExternalRealm",
+    REALM: "DemoRealm",
     BACKEND_URL: "http://localhost:8081",
     SSO_URL: "http://localhost:8080"
 };
